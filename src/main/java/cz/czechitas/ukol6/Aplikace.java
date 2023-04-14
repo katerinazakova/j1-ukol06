50e069384a2e2030853564b21c517168abe80035
7ZKaty <zakova.kz@gmail.com>
2023-04-14 11:02:24 +0200

    Ukol 6 - calculator of geese and rabbits (in progress)

--- src/main/java/cz/czechitas/ukol6/Aplikace.java
+++ src/main/java/cz/czechitas/ukol6/Aplikace.java
@@ -6,8 +6,19 @@ import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.SQLOutput;
 
 public class Aplikace extends JFrame {
+private JLabel geeseLabel;
+private JLabel rabbitsLabel;
+private JLabel numberHeadsLabel;
+private JLabel numberLegsLabel;
+private JTextField geeseField;
+private JTextField rabbitsField;
+private JTextField numberHeadsField;
+private JTextField numberLegsField;
+private JButton calculateButton;
 
     public static void main(String[] args) {
         FlatLightLaf.setup();
@@ -30,9 +41,68 @@ public class Aplikace extends JFrame {
         setLayout(new MigLayout("wrap 2", "[right]rel[50:120:150,grow,fill]"));
         setMinimumSize(new Dimension(250, 200));
 
-        //TODO implementovat formulář podle zadání
+        geeseLabel = new JLabel("Geese");
+        geeseField = new JTextField();
+        geeseLabel.setDisplayedMnemonic('G');
+        geeseLabel.setLabelFor(geeseField);
+        geeseField.setHorizontalAlignment(JTextField.TRAILING);
+        add(geeseLabel);
+        add(geeseField);
+
+        rabbitsLabel = new JLabel("Rabbits");
+        rabbitsField = new JTextField();
+        rabbitsLabel.setDisplayedMnemonic('R');
+        rabbitsLabel.setLabelFor(rabbitsField);
+        rabbitsField.setHorizontalAlignment(JTextField.TRAILING);
+        add(rabbitsLabel);
+        add(rabbitsField);
+
+        add(createButtonCalculator(), "span 2");
+
+        numberHeadsLabel = new JLabel("Number of Heads");
+        numberHeadsField = new JTextField();
+        numberHeadsLabel.setDisplayedMnemonic('H');
+        numberHeadsLabel.setLabelFor(numberHeadsField);
+        numberHeadsField.setHorizontalAlignment(JTextField.TRAILING);
+        add(numberHeadsLabel);
+        add(numberHeadsField);
+
+        numberLegsLabel = new JLabel("Number of Legs");
+        numberLegsField = new JTextField();
+        numberLegsLabel.setDisplayedMnemonic('L');
+        numberLegsLabel.setLabelFor(numberLegsField);
+        numberLegsField.setHorizontalAlignment(JTextField.TRAILING);
+        add(numberLegsLabel);
+        add(numberLegsField);
 
         pack();
+
+        getRootPane().setDefaultButton(calculateButton);
+        calculateButton.addActionListener(this::handleCalculate);
     }
 
+    public JPanel createButtonCalculator(){
+        calculateButton = new JButton("Calculate");
+        calculateButton.setMnemonic('C');
+
+        JPanel buttonCalculator = new JPanel();
+        buttonCalculator.add(calculateButton);
+        return buttonCalculator;
+    }
+    private void handleCalculate(ActionEvent actionEvent){
+        String geese = geeseField.getText();
+        int numberGeese = Integer.parseInt(geese);
+
+        String rabbits = rabbitsField.getText();
+        int numberRabbits = Integer.parseInt(rabbits);
+
+        int heads = numberGeese + numberRabbits;
+        String headsGeeseAndRabbits = Integer.toString(heads);
+
+        int legs = (numberGeese * 2) + (numberRabbits * 4);
+        String legsGeeseAndRabbits = Integer.toString(legs);
+
+        numberHeadsField.setText(headsGeeseAndRabbits);
+        numberLegsField.setText(legsGeeseAndRabbits);
+    }
 }
