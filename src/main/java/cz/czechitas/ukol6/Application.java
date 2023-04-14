1bff6495d257e995d4254c373b346eee4ce89ede
7ZKaty <zakova.kz@gmail.com>
2023-04-14 18:46:38 +0200

    Ukol 6 - calculator of geese and rabbits (bonus - in progress)

--- src/main/java/cz/czechitas/ukol6/Application.java
+++ src/main/java/cz/czechitas/ukol6/Application.java
@@ -11,12 +11,12 @@ import java.awt.event.ActionEvent;
 public class Application extends JFrame {
     private JLabel geeseLabel;
     private JLabel rabbitsLabel;
-    private JLabel numberHeadsLabel;
-    private JLabel numberLegsLabel;
+    private JLabel headsLabel;
+    private JLabel legsLabel;
     private JSpinner geeseSpinner;
     private JSpinner rabbitsSpinner;
-    private JSpinner numberHeadsSpinner;
-    private JSpinner numberLegsSpinner;
+    private JSpinner headsSpinner;
+    private JSpinner legsSpinner;
     private JButton calculateButton;
 
     public static void main(String[] args) {
@@ -41,14 +41,14 @@ public class Application extends JFrame {
         setMinimumSize(new Dimension(250, 200));
 
         geeseLabel = new JLabel("Geese");
-        geeseSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
+        geeseSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 200, 1));
         geeseLabel.setDisplayedMnemonic('G');
         geeseLabel.setLabelFor(geeseSpinner);
         add(geeseLabel);
         add(geeseSpinner);
 
         rabbitsLabel = new JLabel("Rabbits");
-        rabbitsSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
+        rabbitsSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 200, 1));
         rabbitsLabel.setDisplayedMnemonic('R');
         rabbitsLabel.setLabelFor(rabbitsSpinner);
         add(rabbitsLabel);
@@ -56,20 +56,21 @@ public class Application extends JFrame {
 
         add(createButtonCalculator(), "span 2");
 
-        numberHeadsLabel = new JLabel("Number of Heads");
-        numberHeadsSpinner = new JSpinner();
-        numberHeadsLabel.setDisplayedMnemonic('H');
-        numberHeadsLabel.setLabelFor(numberHeadsSpinner);
+        headsLabel = new JLabel("Number of Heads");
+        headsSpinner = new JSpinner();
+        headsLabel.setDisplayedMnemonic('H');
+        headsLabel.setLabelFor(headsSpinner);
+        JFormattedTextField headsGeeseAndRabbits = ((JSpinner.DefaultEditor)headsSpinner.getEditor()).getTextField();
+        headsGeeseAndRabbits.setEditable(false);
+        add(headsLabel);
+        add(headsSpinner);
 
-        add(numberHeadsLabel);
-        add(numberHeadsSpinner);
-
-        numberLegsLabel = new JLabel("Number of Legs");
-        numberLegsSpinner = new JSpinner();
-        numberLegsLabel.setDisplayedMnemonic('L');
-        numberLegsLabel.setLabelFor(numberLegsSpinner);
-        add(numberLegsLabel);
-        add(numberLegsSpinner);
+        legsLabel = new JLabel("Number of Legs");
+        legsSpinner = new JSpinner();
+        legsLabel.setDisplayedMnemonic('L');
+        legsLabel.setLabelFor(legsSpinner);
+        add(legsLabel);
+        add(legsSpinner);
 
         pack();
 
@@ -91,7 +92,13 @@ public class Application extends JFrame {
         int numberRabbits = (Integer) rabbitsSpinner.getValue();
         int numberHeads = numberGeese + numberRabbits;
         int numberLegs = (numberGeese * 2) + (numberRabbits * 4);
-        numberHeadsSpinner.setValue(numberHeads);
-        numberLegsSpinner.setValue(numberLegs);
+        headsSpinner.setValue(numberHeads);
+        legsSpinner.setValue(numberLegs);
+
+        System.out.println("Calculator:");
+        System.out.printf("Number of geese: %d", numberGeese).println();
+        System.out.printf("Number of rabbits: %d", numberRabbits).println();
+        System.out.printf("Number of heads: %d", numberHeads).println();
+        System.out.printf("Number of legs: %d", numberLegs).println();
     }
 }
